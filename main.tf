12b76f4543fb8d51d8f4a18830a5b390f55698ac
stefantopia <kmng@ucla.edu>
2024-01-02 06:56:36 +0000

    fixed dynamic db

--- main.tf
+++ main.tf
@@ -211,9 +211,9 @@ resource "aws_security_group_rule" "tcp_8080" {
 }
 
 resource "aws_dynamodb_table" "company_table" {
-  name        = "company_table-${var.stack_name}
+  name         = "company_table-${var.stack_name}"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key       = "name"
+  hash_key     = "name"
   attribute {
     name = "name"
     type = "S"
